9db6856caa0050801a7d2d53d13228e6f391ab49
HaiTS <transachhai97@gmail.com>
2020-09-09 10:21:41 +0700

    alias

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
+const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 const nextConfig = {
     // Target must be serverless
-    target: 'serverless'
+    target: 'serverless',
+    webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+        // Note: we provide webpack above so you should not `require` it
+
+        const aliases = config.resolve.alias || {};
+        config.resolve.alias = {
+            ...aliases,
+            'private-next-pages': path.resolve(__dirname, 'src/pages'),
+            '@': path.resolve(__dirname, 'src'),
+        };
+
+        // Important: return the modified config
+        return config;
+    },
 };
 
 module.exports = withPlugins([
