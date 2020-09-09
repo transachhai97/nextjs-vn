636dce49853fecce59b800472284f49f1d1b0dde
HaiTS <transachhai97@gmail.com>
2020-09-09 10:12:22 +0700

    pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+const nextConfig = {
     // Target must be serverless
     target: 'serverless'
 };
+
+module.exports = withPlugins([
+    // add plugins here..
+    [
+        withPWA,
+        {
+            pwa: {
+                dest: 'public',
+                runtimeCaching,
+            },
+        },
+    ],
+], nextConfig);
